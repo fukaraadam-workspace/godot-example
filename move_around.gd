81521826366f3028c5a50049ae553916f979e701
Furkan Alan <furkanalan@hotmail.com>
2023-10-30 18:23:35 +0300

    added toggle motion button

--- move_around.gd
+++ move_around.gd
@@ -10,3 +10,7 @@ func _process(delta):
 	rotation += angular_speed * delta
 	var velocity = Vector2.UP.rotated(rotation) * speed
 	position += velocity * delta
+
+
+func _on_button_pressed():
+	set_process(not is_processing())
