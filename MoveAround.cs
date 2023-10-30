0e1f87fea6f96ccece80c243fd6617a3a2313825
Furkan Alan <furkanalan@hotmail.com>
2023-10-30 17:29:57 +0300

    added main screen and renamed scripts

--- MoveAround.cs
+++ MoveAround.cs
@@ -1,11 +1,11 @@
 using Godot;
 
-public partial class ScriptNet : Sprite2D
+public partial class MoveAround : Sprite2D
 {
   private int _speed = 400;
   private float _angularSpeed = Mathf.Pi;
 
-  public ScriptNet()
+  public MoveAround()
   {
     GD.Print("Hello, world!");
   }
