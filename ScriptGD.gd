7b51cd387149fd7917f7db5c3d9d51a670a69210
Furkan Alan <furkanalan@hotmail.com>
2023-10-30 17:21:01 +0300

    added velocity also

--- ScriptGD.gd
+++ ScriptGD.gd
@@ -8,3 +8,5 @@ func _init():
 
 func _process(delta):
 	rotation += angular_speed * delta
+	var velocity = Vector2.UP.rotated(rotation) * speed
+	position += velocity * delta
--- ScriptNet.cs
+++ ScriptNet.cs
@@ -2,16 +2,19 @@ using Godot;
 
 public partial class ScriptNet : Sprite2D
 {
-	private int _speed = 400;
-	private float _angularSpeed = Mathf.Pi;
+  private int _speed = 400;
+  private float _angularSpeed = Mathf.Pi;
 
-	public ScriptNet()
-	{
-		GD.Print("Hello, world!");
-	}
+  public ScriptNet()
+  {
+    GD.Print("Hello, world!");
+  }
 
-	public override void _Process(double delta)
-	{
-		Rotation += _angularSpeed * (float)delta;
-	}
+  public override void _Process(double delta)
+  {
+    Rotation += _angularSpeed * (float)delta;
+
+    var velocity = Vector2.Up.Rotated(Rotation) * _speed;
+    Position += velocity * (float)delta;
+  }
 }
